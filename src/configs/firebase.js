acd5a49ee18e6e64e0bc1228144f48fa3f1eadf2
Kenneth Clausen <me@kjc.dk>
2024-03-11 12:03:23 +0100

    Cleaned firebase config for students

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,14 +1,14 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyArPIFmHtvRAQ_idJKVgsUpRTWB8ejxOMI",
-  authDomain: "fir-test-e3cff.firebaseapp.com",
-  projectId: "fir-test-e3cff",
-  storageBucket: "fir-test-e3cff.appspot.com",
-  messagingSenderId: "674954600282",
-  appId: "1:674954600282:web:db8b13171775aef9459d63",
-  measurementId: "G-G6RLF12EP0"
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: ''
 };
 
 const app = initializeApp(firebaseConfig);
